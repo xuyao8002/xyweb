436587ece0ad6570ab7e7e84b3e247b5762e6eab
xu yao <xu yao>
2017-04-18 21:17:00 +0800

    1

--- src/main/java/com/xuyao/controller/BaseController.java
+++ src/main/java/com/xuyao/controller/BaseController.java
@@ -1,6 +1,11 @@
 package com.xuyao.controller;
 
 import com.xuyao.service.BaseService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +27,32 @@ public class BaseController {
     @ResponseBody
     public String show(){
         baseService.printInfo();
-        return i++ + ", " + baseService.sayHello();
+        return baseService.sayHello(i++);
+    }
+    
+    private static ExecutorService es = Executors.newFixedThreadPool(5);
+    
+    private static AtomicInteger j = new AtomicInteger();
+    
+    public static void main(String[] args){
+    	for(int i = 0; i < 1000 ; i++){
+    		es.execute(new Runnable(){
+
+				@Override
+				public void run() {
+					//j++;
+					j.incrementAndGet();
+				}
+    			
+    		});
+    	}
+    	es.shutdown();
+    	while(true){
+    		if(es.isTerminated()){
+    			System.out.println(j.get());
+    			break;
+    		}
+    	}
     }
 
 }
