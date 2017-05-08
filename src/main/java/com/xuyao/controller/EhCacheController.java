b357797829322940d86776735d8d9d2a706de6c7
xu yao <xu yao>
2017-05-08 22:59:58 +0800

    ehcache

--- src/main/java/com/xuyao/controller/EhCacheController.java
+++ src/main/java/com/xuyao/controller/EhCacheController.java
@@ -1,46 +1,46 @@
 package com.xuyao.controller;
 
-import com.xuyao.proxy.LogHandle;
 import com.xuyao.service.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.lang.reflect.Proxy;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by xuyao on 2017/4/11.
  */
 @Controller
-@RequestMapping("base")
-public class BaseController {
+@RequestMapping("ehcache")
+public class EhCacheController {
 
     @Autowired
     private BaseService baseService;
 
     private int i = 0;
 
-    @RequestMapping("show")
+    @RequestMapping("get/{key}")
     @ResponseBody
-    public String show(){
+    public String get(@PathVariable("key") String key){
 		/*LogHandle handle = new LogHandle(baseService);
 		BaseService bs = (BaseService) Proxy.newProxyInstance(baseService.getClass().getClassLoader(), baseService.getClass().getInterfaces(), handle);
 		bs.printInfo();
 		bs.sayHello();
         return bs.sayHello(i++);*/
-		return baseService.sayHello(i++);
+        //baseService.get(key);
+		return baseService.get(key);//baseService.sayHello(i++);
     }
     
-    @RequestMapping("show1")
+    @RequestMapping("put/{key}/{value}/{i}/{i1}")
     @ResponseBody
-    public String show1(){
+    public String show1(@PathVariable("key") String key, @PathVariable("value") String value, @PathVariable("i") int i, @PathVariable("i1") int i1){
         //baseService.printInfo();
-        return "i'am going to riding";
+        baseService.put(key, value, i, i1);
+        return key + ", " + value;//"i'am going to riding";
     }
 
 
